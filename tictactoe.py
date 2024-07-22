3a0bb7a087eb0df63b9e56f98ff0fda5e2f38926
Juan Calderon <juancalderonmicro@hotmail.com>
2024-07-21 20:47:31 -0500

    Update tictactoe.py
    putting actual information

---
+++ tictactoe.py
@@ -0,0 +1,71 @@
+import random
+# import os 
+
+# def inicializar_juego():
+#     "Funcino que inicia valores del juego"
+#     juego_en_curso = true 
+#     jugadores = [[input = ("Jugador1: "), "X"], [input = ("Jugador2: ")]]
+#     jugador_actual = random.randient(0,1)
+#     tablero = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+#     return juego_en_curso, jugadores, jugador_actual, tablero
+
+# def actualizar_tablero(jugador, cordenada_fila, cordenada_columna, tablero_actual):
+#     "Actualizar el tablero con la accion de lel jugador actual"
+#     tablero_actual[cordenada_fila - 1][cordenada_columna -1] = jugador[1]
+#     return tablero_actual
+
+# def tablero_completo(tablero_actual):
+#     "Comprueba si el tablero esta completo, debolver true or false"
+#     for linea in tablero_actual:
+#         for celda in linea:
+#             if celda == "-":
+#                 return false
+#     return true 
+
+# def comprobar_ganador(jugador, tablero_actual)
+#     "Para comprobar si ha ganado el jugador actua, responder  con true or false"
+#     #Comprobar por filas
+#     for i in range(3):
+#         ganador = true
+#         for x in range(3):
+#             if tablero_actual[i][x] != jugador[1]:
+#                 ganador = False 
+#                 break
+#         if ganador:
+#             return ganador 
+        
+#     #Comprobar por columna
+#     for i in range(3):
+#         ganador = true
+#         for x in range(3):
+#             if tablero_actual[i][x] != jugador[1]:
+#                 ganador = False 
+#                 break
+#         if ganador:
+#             return ganador 
+    
+#     #Comprobar por diagonales
+#     ganador = true
+#     for i in range(3):
+#         if tablero_actual[i][i] != jugador[1]:
+#          ganador = False 
+#          break
+#     if ganador:
+#         return ganador
+
+#     ganador = True 
+    
+# for i in range(3):
+#         if tablero_actual[i][3 - 1 - 1] != jugador[1]:
+#          ganador = False 
+#          break
+#     if ganador:
+        
+#     return ganador
+    
+    
+random.randrange(1, 10)
+    
+        
+        
+
